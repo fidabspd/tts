7bef66771134146234d4f91c475b8e936f90ad21
fidabspd <fidabspd@gmail.com>
2022-04-19 23:36:29 +0900

    Apply validation set

--- codes/train_tts.py
+++ codes/train_tts.py
@@ -1,13 +1,14 @@
 import os
 import math
 import argparse
+import collections
 from tqdm import tqdm
 
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 
-from configs import *
+import configs as cf
 from preprocess import *
 from transformer_torch import *
 
@@ -19,6 +20,99 @@ def parse_args():
     parser.add_argument('--configs_file_name', type=str, default='./hyperparams.json')
 
     return parser.parse_args()
+
+
+class TextMelDataset(Dataset):
+    
+    def __init__(self, script_path, sheet_name, audio_path, sr, n_mels, n_fft,
+                 hop_length, win_length):
+        self.scripts = load_script(script_path, sheet_name)
+        self.audio_path = audio_path
+        self.sr = sr
+        self.n_mels = n_mels
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.win_length = win_length
+
+        self.audio_file_list = os.listdir(self.audio_path)
+    
+    def __len__(self):
+        return len(self.audio_file_list)
+    
+    def __getitem__(self, idx):
+        file_name = self.audio_file_list[idx]
+        file_num = int(file_name[:-4])
+
+        # script
+        script = self.scripts.query(f'Index == {file_num}')['대사'].tolist()[0]
+        script = normalize_text(script)
+        text_tokens = tokenize(script, as_id=True)
+        
+        # audio
+        fpath = os.path.join(self.audio_path, str(file_num)+'.wav')
+        mel = get_mel(fpath, self.sr, self.n_mels, self.n_fft, self.hop_length, self.win_length)
+        mel = np.concatenate([
+                np.zeros([1, self.n_mels], np.float32),
+                mel,
+                np.zeros([2, self.n_mels], np.float32)
+        ], axis=0)  # <sos> + mel + <eos>
+        
+        return {'text_tokens': text_tokens, 'mel': mel, 'text_tokens_len': len(text_tokens), 'mel_len': len(mel)}
+
+
+def get_single_speaker_dataset(speaker, wav_path, script_path,
+                               sr, n_mels, n_fft, hop_length, win_length):
+
+    data_list = get_data_list(speaker, wav_path)
+
+    concat_dataset = []
+    print(f'Loading {data_list} ...')
+    for sheet_name in data_list:
+        sheet_name = sheet_name.split('_')[1]
+        
+        audio_path = os.path.join(wav_path, speaker+'_'+sheet_name)
+        
+        text_mel_dataset = TextMelDataset(
+            script_path, sheet_name, audio_path, sr, n_mels, n_fft,
+            hop_length, win_length)
+        concat_dataset.append(text_mel_dataset)
+        print(f'{sheet_name} Done!')
+        
+    return ConcatDataset(concat_dataset)
+
+
+def collate_fn(batch):
+
+    def pad_tokens(text_tokens, max_len):
+        len_tokens = len(text_tokens)
+        return np.pad(text_tokens, (0, max_len-len_tokens))
+
+    def pad_mel(mel, max_len):
+        len_mel = len(mel)
+        return np.pad(mel, ((0, max_len-len_mel), (0, 0)))
+
+    max_text_len = max([data['text_tokens_len'] for data in batch])
+    max_speech_len = max([data['mel_len'] for data in batch])
+
+    text_tokens = np.stack([
+        pad_tokens(data['text_tokens'], max_text_len)
+        for data in batch
+    ])
+    mel = np.stack([
+        pad_mel(data['mel'], max_speech_len)
+        for data in batch
+    ])
+
+    return {
+        'text_tokens': torch.LongTensor(text_tokens),
+        'mel': torch.FloatTensor(mel)
+    }
+
+
+def get_dl_by_ds(ds, batch_size, shuffle=False):
+    dl = DataLoader(
+        ds, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+    return dl
 
 
 def create_tensorboard_graph(model, inputs, path):
@@ -44,149 +138,80 @@ def initialize_weights(m):
         nn.init.xavier_uniform_(m.weight.data)
 
 
-# def train_one_epoch(model, dl, optimizer, criterion, clip, device):
-
-#     n_data = len(dl.dataset)
-#     train_loss = 0
-#     n_processed_data = 0
-
-#     model.train()
-#     pbar = tqdm(dl)
-#     for batch in pbar:
-#         tensor_dict_to_device(batch, device)
-#         inputs, targets = batch['inputs'], batch['targets']
-#         n_processed_data += len(targets)
-
-#         pred = model(**inputs)
-#         loss = criterion(pred, targets)
-#         train_loss += loss.item()/n_data
-
-#         optimizer.zero_grad()
-#         loss.backward()
-#         nn.utils.clip_grad_norm_(model.parameters(), clip)
-#         optimizer.step()
-
-#         train_loss_tmp = train_loss*n_data/n_processed_data
-#         pbar.set_description(
-#             f'Train Loss: {train_loss_tmp:9.6f} | {n_processed_data:6d}/{n_data:6d} ')
-
-#     return train_loss
+def tensor_dict_to_device(tensor_dict, device):
+    
+    assert isinstance(tensor_dict, collections.abc.Mapping),\
+        f'tensor_dict is not dicts. Found {type(tensor_dict)}.'
+    
+    for k, v in tensor_dict.items():
+        if isinstance(v, collections.abc.Mapping):
+            tensor_dict_to_device(v, device)
+        elif isinstance(v, torch.Tensor):
+            tensor_dict[k] = v.to(device)
+        else:
+            raise Exception(f'{v} is not torch.Tensor')
 
 
 def train_one_epoch(model, dl, optimizer, criterion, clip, device):
 
     n_data = len(dl.dataset)
-    train_loss = 0; n_processed_data = 0
+    train_loss = 0
+    n_processed_data = 0
+
     model.train()
     pbar = tqdm(dl)
-    for inp, tar in pbar:
-        n_processed_data += len(inp)
-        inp, tar = inp.to(device), tar.to(device)
+    for batch in pbar:
+        tensor_dict_to_device(batch, device)
+        text_tokens, mel = batch['text_tokens'], batch['mel']
+        n_processed_data += len(mel)
 
-        outputs, _, _ = model(inp, tar[:,:-1])
-
-        outputs = outputs.contiguous().view(-1)
-        tar = tar[:,1:].contiguous().view(-1)
-        loss = criterion(outputs, tar)
+        mel_pred, _, _ = model(text_tokens, mel[:,:-1])
+        mel_pred = mel_pred.contiguous().view(-1)
+        mel = mel[:,1:].contiguous().view(-1)
+        loss = criterion(mel_pred, mel)
         train_loss += loss.item()/n_data
 
         optimizer.zero_grad()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
 
         train_loss_tmp = train_loss*n_data/n_processed_data
         pbar.set_description(
-            f'{n_processed_data:4d}/{n_data:4d} | loss: {train_loss_tmp:10.6f}')
+            f'Train Loss: {train_loss_tmp:9.6f} | {n_processed_data:6d}/{n_data:6d} ')
 
     return train_loss
 
 
-# def evaluate(model, dl, criterion, device):
-
-#     n_data = len(dl.dataset)
-#     valid_loss = 0
-#     n_processed_data = 0
-
-#     model.eval()
-#     pbar = tqdm(dl)
-#     with torch.no_grad():
-#         for batch in pbar:
-#             tensor_dict_to_device(batch, device)
-#             inputs, targets = batch['inputs'], batch['targets']
-#             n_processed_data += len(targets)
-            
-#             pred = model(**inputs)
-#             loss = criterion(pred, targets)
-#             valid_loss += loss.item()/n_data
-
-#             valid_loss_tmp = valid_loss*n_data/n_processed_data
-#             pbar.set_description(
-#                 f'Valid Loss: {valid_loss_tmp:9.6f} | {n_processed_data:6d}/{n_data:6d} ')
-
-#     return valid_loss
-
-
 def evaluate(model, dl, criterion, device):
+
     n_data = len(dl.dataset)
-    
     valid_loss = 0
+    n_processed_data = 0
 
     model.eval()
+    pbar = tqdm(dl)
     with torch.no_grad():
-        for inp, tar in dl:
-            inp, tar = inp.to(device), tar.to(device)
-            outputs, _, _ = model(inp, tar[:,:-1])
-
-            output_dim = outputs.shape[-1]
-
-            outputs = outputs.contiguous().view(-1, output_dim)
-            tar = tar[:,1:].contiguous().view(-1)
-            loss = criterion(outputs, tar)
-
+        for batch in pbar:
+            tensor_dict_to_device(batch, device)
+            text_tokens, mel = batch['text_tokens'], batch['mel']
+            n_processed_data += len(mel)
+            
+            mel_pred, _, _ = model(text_tokens, mel[:,:-1])
+            mel_pred = mel_pred.contiguous().view(-1)
+            mel = mel[:,1:].contiguous().view(-1)
+            loss = criterion(mel_pred, mel)
             valid_loss += loss.item()/n_data
+
+            valid_loss_tmp = valid_loss*n_data/n_processed_data
+            pbar.set_description(
+                f'Valid Loss: {valid_loss_tmp:9.6f} | {n_processed_data:6d}/{n_data:6d} ')
 
     return valid_loss
 
 
-# def train_model(model, train_dl, valid_dl, optimizer, criterion, n_epochs,
-#                 es_patience, clip, model_file_path, train_log_path, device):
-#     if train_log_path is not None:
-#         writer = SummaryWriter(train_log_path)
-#     best_train_loss = float('inf')
-#     best_valid_loss = float('inf')
-#     best_epoch = 0
-
-#     for epoch in range(n_epochs):
-
-#         print(f'Epoch: {epoch+1}/{n_epochs}')
-#         train_loss = train_one_epoch(model, train_dl, optimizer, criterion, clip, device)
-#         valid_loss = evaluate(model, valid_dl, criterion, device)
-#         if train_log_path is not None:
-#             writer.add_scalars('loss', {
-#                     'train_loss':train_loss,
-#                     'valid_loss':valid_loss,
-#                 }, epoch+1)
-
-#         if valid_loss < best_valid_loss:
-#             print('Best!\n')
-#             best_epoch = epoch
-#             best_train_loss = train_loss
-#             best_valid_loss = valid_loss
-#             torch.save(model, model_file_path)
-
-#         if epoch-best_epoch >= es_patience:
-#             print(f'\nBest Epoch: {best_epoch+1:02}')
-#             print(f'\tBest Train Loss: {best_train_loss:.3f}')
-#             print(f'\tBest Validation Loss: {best_valid_loss:.3f}')
-#             break
-    
-#     if train_log_path is not None:
-#         writer.close()
-
-
-def train(model, n_epochs, es_patience, train_dl, valid_dl, optimizer,
-          criterion, clip, device, model_path, train_log_path, model_name='chatbot'):
+def train_model(model, train_dl, valid_dl, optimizer, criterion, n_epochs,
+                es_patience, clip, model_file_path, train_log_path, device):
     if train_log_path is not None:
         writer = SummaryWriter(train_log_path)
     best_train_loss = float('inf')
@@ -197,34 +222,24 @@ def train(model, n_epochs, es_patience, train_dl, valid_dl, optimizer,
 
         print(f'Epoch: {epoch+1}/{n_epochs}')
         train_loss = train_one_epoch(model, train_dl, optimizer, criterion, clip, device)
+        valid_loss = evaluate(model, valid_dl, criterion, device)
         if train_log_path is not None:
-            writer.add_scalar('train loss', train_loss, epoch)
-        if valid_dl is not None:
-            valid_loss = evaluate(model, valid_dl, criterion, device)
-            if train_log_path is not None:
-                writer.add_scalar('valid loss', valid_loss, epoch)
+            writer.add_scalars('loss', {
+                    'train_loss':train_loss,
+                    'valid_loss':valid_loss,
+                }, epoch+1)
 
-
-        if valid_dl is not None:
-            if valid_loss < best_valid_loss:
-                best_epoch = epoch
-                print('Best!')
-                best_valid_loss = valid_loss
-                torch.save(model, model_path+model_name+'.pt')
-        else:
-            if train_loss < best_train_loss:
-                best_epoch = epoch
-                print('Best!')
-                best_train_loss = train_loss
-                torch.save(model, model_path+model_name+'.pt')
-
-        if valid_dl is not None:
-            print(f'Valid Loss: {valid_loss:.3f}')
+        if valid_loss < best_valid_loss:
+            print('Best!\n')
+            best_epoch = epoch
+            best_train_loss = train_loss
+            best_valid_loss = valid_loss
+            torch.save(model, model_file_path)
 
         if epoch-best_epoch >= es_patience:
             print(f'\nBest Epoch: {best_epoch+1:02}')
-            print(f'\tBest Train Loss: {train_loss:.3f}')
-            print(f'\tBest Validation Loss: {valid_loss:.3f}')
+            print(f'\tBest Train Loss: {best_train_loss:.3f}')
+            print(f'\tBest Validation Loss: {best_valid_loss:.3f}')
             break
     
     if train_log_path is not None:
@@ -243,15 +258,28 @@ def main(args):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f'Using device {device}')
 
-    # Load data
-    ds = get_single_speaker_dataset(
-        SPEAKER, WAV_PATH, SCRIPT_FILE_NAME, SR, N_MELS, N_FFT, HOP_LENGTH, WIN_LENGTH)
-    train_dl = DataLoader(ds, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
+    # Set Dataset
+    full_ds = get_single_speaker_dataset(
+        cf.SPEAKER, cf.WAV_PATH, cf.SCRIPT_FILE_NAME,
+        cf.SR, cf.N_MELS, cf.N_FFT, cf.HOP_LENGTH, cf.WIN_LENGTH
+    )
+
+    TRAINSET_RATIO = 0,85
+    # Split train, valid
+    data_length = len(full_ds)
+    train_ds_len = int(data_length*TRAINSET_RATIO)
+    valid_ds_len = data_length - train_ds_len
+    train_ds, valid_ds = random_split(full_ds, [train_ds_len, valid_ds_len])
+
+    # Set DataLoader
+    train_dl = get_dl_by_ds(train_ds, BATCH_SIZE, shuffle=False)
+    valid_dl = get_dl_by_ds(valid_ds, BATCH_SIZE, shuffle=False)
 
     # Set model
     transformer = Transformer(
-        len(ALL_SYMBOLS), N_MELS, N_LAYERS, HIDDEN_DIM, N_HEADS, PF_DIM,
-        TEXT_SEQ_LEN, SPEECH_SEQ_LEN, PAD_IDX, DROPOUT_RATIO, device
+        len(cf.ALL_SYMBOLS), cf.N_MELS, cf.N_LAYERS, cf.HIDDEN_DIM,
+        cf.N_HEADS, cf.PF_DIM, cf.TEXT_SEQ_LEN, cf.SPEECH_SEQ_LEN,
+        cf.PAD_IDX, cf.DROPOUT_RATIO, device
     ).to(device)
 
     print(f'# of trainable parameters: {count_parameters(transformer):,}')
@@ -267,7 +295,7 @@ def main(args):
 
     if not VALIDATE:
         valid_dl = None
-    train(
+    train_model(
         transformer, N_EPOCHS, ES_PATIENCE, train_dl, valid_dl,
         optimizer, criterion, CLIP, device, MODEL_PATH, TRAIN_LOG_PATH, MODEL_NAME
     )
